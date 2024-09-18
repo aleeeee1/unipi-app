3ab9aaed5ac6e7a875664d4afc0deb20cc96aba5
aleeeee1 <pace.alessandro05@protonmail.com>
2024-09-18 08:34:41 +0200

    feat: added timeline and linked it with page view

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,7 +1,9 @@
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:get/get.dart';
 import 'package:unipi_orario/services/internal_api.dart';
+import 'package:easy_date_timeline/easy_date_timeline.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +14,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   InternalAPI internalAPI = Get.find<InternalAPI>();
+
+  final EasyInfiniteDateTimelineController _controller = EasyInfiniteDateTimelineController();
+  DateTime currentDate = DateTime.now();
+
+  late PageController _pageController;
+  int currentPageValue = 10000;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: currentPageValue);
+  }
 
   PreferredSizeWidget appBar() {
     return AppBar(
@@ -71,11 +85,127 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget timeLine() {
+    TextStyle subStyle = TextStyle(
+      fontSize: Theme.of(context).textTheme.labelSmall?.fontSize,
+      color: Theme.of(context).disabledColor,
+      fontWeight: FontWeight.w600,
+    );
+
+    DayStyle dayStyle = DayStyle(
+      monthStrStyle: subStyle,
+      dayStrStyle: subStyle,
+      dayNumStyle: TextStyle(
+        fontSize: Theme.of(context).textTheme.headlineSmall?.fontSize,
+        color: Theme.of(context).colorScheme.onSurface,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+
+    List<String> months = [
+      FlutterI18n.translate(context, "months.january"),
+      FlutterI18n.translate(context, "months.february"),
+      FlutterI18n.translate(context, "months.march"),
+      FlutterI18n.translate(context, "months.april"),
+      FlutterI18n.translate(context, "months.may"),
+      FlutterI18n.translate(context, "months.june"),
+      FlutterI18n.translate(context, "months.july"),
+      FlutterI18n.translate(context, "months.august"),
+      FlutterI18n.translate(context, "months.september"),
+      FlutterI18n.translate(context, "months.october"),
+      FlutterI18n.translate(context, "months.november"),
+      FlutterI18n.translate(context, "months.december"),
+    ];
+
+    return EasyInfiniteDateTimeLine(
+      controller: _controller,
+      //
+      firstDate: DateTime(DateTime.september),
+      lastDate: DateTime(DateTime.now().year).add(const Duration(days: 365 * 4)),
+      focusDate: currentDate,
+      //
+      onDateChange: (selectedDate) {
+        setState(() {
+          currentDate = selectedDate;
+          _pageController.jumpToPage(currentPageValue + currentDate.difference(DateTime.now()).inDays);
+        });
+      },
+      activeColor: Theme.of(context).colorScheme.primaryContainer,
+      locale: Localizations.localeOf(context).toLanguageTag(),
+      dayProps: EasyDayProps(
+        todayHighlightStyle: TodayHighlightStyle.withBackground,
+        todayHighlightColor: Theme.of(context).colorScheme.tertiaryContainer,
+        //
+        activeDayStyle: dayStyle,
+        todayStyle: dayStyle,
+        inactiveDayStyle: dayStyle,
+        //
+        height: 80,
+      ),
+      headerBuilder: (context, date) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10,
+            vertical: 10,
+          ),
+          child: Row(
+            children: [
+              Text(
+                "${months[currentDate.month - 1]} ${currentDate.year}",
+                style: TextStyle(
+                  fontSize: Theme.of(context).textTheme.headlineSmall?.fontSize,
+                  color: Theme.of(context).colorScheme.onSurface,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget eventsList() {
+    return Expanded(
+      child: PageView.builder(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        itemBuilder: (context, index) {
+          DateTime date = DateTime.now().add(Duration(days: index - currentPageValue));
+
+          return Center(
+            child: Text(
+              date.toString(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _onPageChanged(int page) {
+    setState(() {
+      currentDate = DateTime.now().add(Duration(days: page - currentPageValue));
+      _controller.animateToDate(currentDate);
+    });
+  }
+
+  Widget body() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        timeLine(),
+        eventsList(),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ThemeSwitchingArea(
       child: Scaffold(
         appBar: appBar(),
+        body: body(),
       ),
     );
   }
